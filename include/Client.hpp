2734d3ae3c2e21de3a99e7a31df2d77d32cb7ad8
VioCio <viorel.maravela29@gmail.com>
2025-05-28 07:26:28 -0700

    Continua lucrul la client

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,10 +1,40 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cerrno>
+#include <cstring>
+
 namespace client
 {
     class Client
     {
+        int sock = 0;
+
+    public:
+        Client()
+        {
+            sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // creare socket
+            if (sock == -1)
+            {
+                throw std::runtime_error("Eroare la crearea socketului");
+            }
+        }
+
+        int connectToServer(sockaddr *addr)
+        {
+            if (connect(sock, addr, sizeof(sockaddr_in)) == -1)
+            {
+                throw std::runtime_error(std::string("Eroare la connect: ") + strerror(errno));
+            }
+            return 0;
+        }
+
+        int getSock()
+        {
+            return sock;
+        }
     };
 }
 
--- src/client.cpp
+++ src/client.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include "Client.hpp"
+#include <arpa/inet.h>
+#include <string.h>
+
+void writeEndpointAddress(sockaddr_in &endpoint, int port_) // acceasi functie ca si in Server.hpp
+{
+    endpoint.sin_family = AF_INET;                     // familia - IPv4
+    endpoint.sin_port = htons(port_);                  // portul
+    endpoint.sin_addr.s_addr = inet_addr("127.0.0.1"); // adresa IP
+    memset(endpoint.sin_zero, 0, sizeof(endpoint.sin_zero));
+}
 
 int main()
 {
-    std::cout << "DE implementat" << std::endl;
+    client::Client clientHTTP;
+    std::cout << clientHTTP.getSock() << std::endl;
+    sockaddr_in adresaServer{};
+    writeEndpointAddress(adresaServer, 55555);
+    std::cout << clientHTTP.connectToServer((sockaddr *)&adresaServer) << std::endl;
 }
